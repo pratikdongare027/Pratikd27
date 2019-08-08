e0964295e42c579c2dfea71785aa6d73fa4b0860
pratikdongare027 <53899724+pratikdongare027@users.noreply.github.com>
2019-08-08 15:47:16 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,13 @@
+cn=(input('Enter Customer Name'))
+u=float(input('Enter the number of units of electricity consumed'))
+
+if u>500:
+    print('The electricity charges are 3000 and delivery charges are 50.')
+if 100<u<200:
+    print('The electricity charges are 500 and delivery charges are 50.')
+if 200<u<300:
+    print('The electricity charges are 650 and delivery charges are 50.')
+if 300<u<500:
+    print('The electricity charges are 800 and delivery charges are 50.')
+            
+        
